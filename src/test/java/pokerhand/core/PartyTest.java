384dca51edc3a3e613e042020db5e6bba41e4393
Apoorva64 <appadooapoorva@gmail.com>
2022-10-14 17:24:57 +0200

    test: add tests for party

---
+++ src/test/java/pokerhand/core/PartyTest.java
@@ -0,0 +1,38 @@
+package pokerhand.core;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PartyTest {
+
+    @Nested
+    @DisplayName("Tests the winner for a party with two hand and a single card per hand")
+    class TestWinnerWithOneCard {
+        @Test
+        void TestEqualHandType() {
+            Party party = new Party(new Hand(new Card(CardValue.ACE)), new Hand(new Card(CardValue.ACE)));
+            assertEquals(party.getWinner(), party.getWinner());
+        }
+
+        @Nested
+        @DisplayName("Tests the winner for a party with two hand and a single card per hand")
+        class TestDifferentHandType {
+            @Test
+            void testSmallerHandType() {
+                Party party = new Party(new Hand(new Card(CardValue.ACE)), new Hand(new Card(CardValue.KING)));
+                assertEquals(party.getHands().get(0), party.getWinner());
+            }
+
+            @Test
+            void testBiggerHandType() {
+                Party party = new Party(new Hand(new Card(CardValue.KING)), new Hand(new Card(CardValue.ACE)));
+                assertEquals(party.getHands().get(1), party.getWinner());
+            }
+        }
+
+    }
+
+}
