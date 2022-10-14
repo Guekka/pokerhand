8a75bc292c65e1a4ab37018274a81ca63ca19823
loicpalayer <84199585+loicpalayer@users.noreply.github.com>
2022-10-14 16:19:00 +0200

    feat: add getWinner in Party

--- src/main/java/pokerhand/core/Party.java
+++ src/main/java/pokerhand/core/Party.java
@@ -1,6 +1,7 @@
 package pokerhand.core;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Party {
@@ -10,6 +11,11 @@ public class Party {
         this.hands = new ArrayList<>();
         this.hands.add(hand1);
         this.hands.add(hand2);
+    }
+
+    public Hand getWinner() {
+        // get maximum element
+        return hands.stream().max(Comparator.comparing(Hand::getPower)).orElseThrow();
     }
 
     public List<Hand> getHands() {
