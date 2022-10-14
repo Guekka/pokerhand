a206e2f83a25c353f27571357d818ee839c91bfe
Edgar B <edgar.bizel@etu.univ-cotedazur.fr>
2022-10-14 15:48:56 +0200

    style: rename variable

--- src/main/java/pokerhand/core/Power.java
+++ src/main/java/pokerhand/core/Power.java
@@ -8,22 +8,22 @@ import java.util.Objects;
 public record Power(HandType handType, List<CardValue> secondary) implements Comparable<Power> {
 
     @Override
-    public int compareTo(Power power) {
-        //if the handType power is different, higher handType power wins
-        if (this.handType.compareTo(power.handType) > 0)
+    public int compareTo(Power other) {
+        // if the handType is different, higher handType wins
+        if (this.handType.compareTo(other.handType) > 0)
             return 1;
 
-        if (this.handType.compareTo(power.handType) < 0)
+        if (this.handType.compareTo(other.handType) < 0)
             return -1;
 
-        //if the primary power is the same, we can compare the secondary power
+        // if the primary power is the same, we can compare the secondary power
         var comparator = new ListComparator<CardValue>();
-        return comparator.compare(this.secondary, power.secondary);
+        return comparator.compare(this.secondary, other.secondary);
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Power power) {
+    public boolean equals(Object other) {
+        if (other instanceof Power power) {
             return this.compareTo(power) == 0;
         }
         return false;
