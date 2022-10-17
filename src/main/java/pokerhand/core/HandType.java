dbbc7ed731a4343a48e43ce702f6a2514dc79b16
Apoorva64 <appadooapoorva@gmail.com>
2022-10-17 16:10:17 +0200

    docs: fixed HandType docs

--- src/main/java/pokerhand/core/HandType.java
+++ src/main/java/pokerhand/core/HandType.java
@@ -11,21 +11,25 @@ public enum HandType {
     HIGH_CARD,
     /**
      * The hand is a pair.
-     */
-    PAIR,
-    /**
-     * The hand is two pairs.
      * One pair, or simply a pair,
      * is a hand that contains two cards of one rank and three cards of three other ranks (the kickers),
      * such as 4♥ 4♠ K♠ 10♦ 5♠ ("one pair, fours" or a "pair of fours").
      * It ranks below two pair and above high card.
      */
-    TWO_PAIR,
+    PAIR,
     /**
-     * The hand is three of a kind.
+     * The hand is two pairs.
      * Two pair is a hand that contains two cards of one rank,
      * two cards of another rank and one card of a third rank (the kicker),
      * such as J♥ J♣ 4♣ 4♠ 9♥ ("two pair, jacks and fours" or "two pair, jacks over fours" or "jacks up").
+     */
+    TWO_PAIR,
+    /**
+     * The hand is three of a kind.
+     * Three of a kind, trips or a set is a hand that contains three cards of one rank and two cards of two other ranks
+     * (the kickers),
+     * such as 9♣ 9♠ 9♦ 7♠ 3♣ ("three of a kind, nines" or "set of nines").
+     * It ranks below a straight and above two pair.
      */
     THREE_OF_A_KIND,
     /**
@@ -51,9 +55,10 @@ public enum HandType {
     FULL_HOUSE,
     /**
      * The hand is four of a kind.
-     * A full house is a hand that contains any three cards of the same rank together with any two cards of the
-     * same rank. For example, 3♣ 3♠ 3♦ 6♣ 6♥ ("three of a kind, threes full of sixes").
-     *
+     * Four of a kind, quads or quads is a hand that contains four cards of one rank and one card of another rank
+     * (the kicker),
+     * such as 2♣ 2♠ 2♦ 2♥ 3♣ ("four of a kind, twos" or "quads of twos").
+     * It ranks below a straight flush and above a full house.
      */
     FOUR_OF_A_KIND,
     /**
