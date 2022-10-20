6773844fdff75925fb4c1aa0c4c6bc2f05defee6
loicpalayer <loic.palayer@etu.univ-cotedazur.fr>
2022-10-20 16:11:57 +0200

    test: add five cards hands test for Party

--- src/test/java/pokerhand/core/PartyTest.java
+++ src/test/java/pokerhand/core/PartyTest.java
@@ -4,36 +4,44 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PartyTest {
 
     @Nested
-    @DisplayName("Tests the winner for a party with two hand and a single card per hand")
-    class TestWinnerWithOneCard {
+    @DisplayName("Tests the winner for a party with two hands" )
+    class TestWinner {
         @Test
         void TestEqualHandType() {
-            Party party = new Party(new Hand(List.of(new Card(CardValue.ACE))), new Hand(List.of(new Card(CardValue.ACE))));
-            assertEquals(party.getWinner(), party.getWinner());
+            Party party1 = new Party(new Hand(List.of(new Card(CardValue.ACE))), new Hand(List.of(new Card(CardValue.ACE))));
+            assertEquals(party1.getWinner(), party1.getWinner());
+
+            //same test with 5 cards per hand
+            Party party2 = new Party(new Hand(List.of(new Card(CardValue.ACE), new Card(CardValue.KING), new Card(CardValue.QUEEN), new Card(CardValue.JACK), new Card(CardValue.TEN))), new Hand(List.of(new Card(CardValue.ACE), new Card(CardValue.KING), new Card(CardValue.QUEEN), new Card(CardValue.JACK), new Card(CardValue.TEN))));
+            assertEquals(party2.getWinner(), party2.getWinner());
         }
 
         @Nested
-        @DisplayName("Tests the winner for a party with two hand and a single card per hand")
+        @DisplayName("Tests the winner for a party with two hands" )
         class TestDifferentHandType {
             @Test
             void testSmallerHandType() {
-                Party party = new Party(new Hand(List.of(new Card(CardValue.ACE))), new Hand(List.of(new Card(CardValue.KING))));
-                assertEquals(party.getHands().get(0), party.getWinner());
+                Party party1 = new Party(new Hand(List.of(new Card(CardValue.ACE))), new Hand(List.of(new Card(CardValue.KING))));
+                assertEquals(party1.getHands().get(0), party1.getWinner());
+
+                Party party2 = new Party(new Hand(List.of(new Card(CardValue.ACE), new Card(CardValue.KING), new Card(CardValue.QUEEN), new Card(CardValue.JACK), new Card(CardValue.TEN))), new Hand(List.of(new Card(CardValue.ACE), new Card(CardValue.KING), new Card(CardValue.QUEEN), new Card(CardValue.JACK), new Card(CardValue.NINE))));
+                assertEquals(party2.getHands().get(0), party2.getWinner());
             }
 
             @Test
             void testBiggerHandType() {
-                Party party = new Party(new Hand(List.of(new Card(CardValue.KING))), new Hand(List.of(new Card(CardValue.ACE))));
-                assertEquals(party.getHands().get(1), party.getWinner());
+                Party party1 = new Party(new Hand(List.of(new Card(CardValue.KING))), new Hand(List.of(new Card(CardValue.ACE))));
+                assertEquals(party1.getHands().get(1), party1.getWinner());
+
+                Party party2 = new Party(new Hand(List.of(new Card(CardValue.ACE), new Card(CardValue.KING), new Card(CardValue.QUEEN), new Card(CardValue.JACK), new Card(CardValue.NINE))), new Hand(List.of(new Card(CardValue.ACE), new Card(CardValue.KING), new Card(CardValue.QUEEN), new Card(CardValue.JACK), new Card(CardValue.TEN))));
+                assertEquals(party2.getHands().get(1), party2.getWinner());
             }
         }
 
