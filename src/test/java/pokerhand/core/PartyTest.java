8cc9e0488a9098bc34c52b21622795c0c1015e5e
Edgar B <edgar.bizel@etu.univ-cotedazur.fr>
2022-10-23 22:16:53 +0200

    test: improve coverage

--- src/test/java/pokerhand/core/PartyTest.java
+++ src/test/java/pokerhand/core/PartyTest.java
@@ -20,6 +20,8 @@ class PartyTest {
 
             //same test with 5 cards per hand
             Party party2 = new Party(new Hand(List.of(new Card(CardValue.ACE, CardColor.CLUB), new Card(CardValue.KING, CardColor.CLUB), new Card(CardValue.QUEEN, CardColor.CLUB), new Card(CardValue.JACK, CardColor.CLUB), new Card(CardValue.TEN, CardColor.CLUB))), new Hand(List.of(new Card(CardValue.ACE, CardColor.CLUB), new Card(CardValue.KING, CardColor.CLUB), new Card(CardValue.QUEEN, CardColor.CLUB), new Card(CardValue.JACK, CardColor.CLUB), new Card(CardValue.TEN, CardColor.CLUB))));
+
+
             assertEquals(party2.getWinner(), party2.getWinner());
         }
 
@@ -42,6 +44,29 @@ class PartyTest {
 
                 Party party2 = new Party(new Hand(List.of(new Card(CardValue.ACE, CardColor.CLUB), new Card(CardValue.KING, CardColor.CLUB), new Card(CardValue.QUEEN, CardColor.CLUB), new Card(CardValue.JACK, CardColor.CLUB), new Card(CardValue.NINE, CardColor.CLUB))), new Hand(List.of(new Card(CardValue.ACE, CardColor.CLUB), new Card(CardValue.KING, CardColor.CLUB), new Card(CardValue.QUEEN, CardColor.CLUB), new Card(CardValue.JACK, CardColor.CLUB), new Card(CardValue.TEN, CardColor.CLUB))));
                 assertEquals(party2.getHands().get(1), party2.getWinner());
+            }
+
+            @Test
+            void testFlushHandType(){
+                // flush, not straight
+                var flushHand = new Hand(List.of(new Card(CardValue.TWO, CardColor.CLUB), new Card(CardValue.THREE, CardColor.CLUB), new Card(CardValue.FOUR, CardColor.CLUB), new Card(CardValue.FIVE, CardColor.CLUB), new Card(CardValue.SEVEN, CardColor.CLUB)));
+                // best card
+                var bestCardHand = new Hand(List.of(new Card(CardValue.ACE, CardColor.CLUB), new Card(CardValue.TWO, CardColor.SPADE), new Card(CardValue.THREE, CardColor.HEART), new Card(CardValue.JACK, CardColor.CLUB), new Card(CardValue.NINE, CardColor.CLUB)));
+
+                var party = new Party(flushHand, bestCardHand);
+                assertEquals(flushHand, party.getWinner());
+
+            }
+
+            @Test
+            void testFlushSecondary(){
+                // flush, not straight
+                var flushHand = new Hand(List.of(new Card(CardValue.TWO, CardColor.CLUB), new Card(CardValue.FIVE, CardColor.CLUB), new Card(CardValue.SIX, CardColor.CLUB), new Card(CardValue.SEVEN, CardColor.CLUB), new Card(CardValue.NINE, CardColor.CLUB)));
+                // better flush, not straight
+                var betterFlushHand = new Hand(List.of(new Card(CardValue.TWO, CardColor.CLUB), new Card(CardValue.FOUR, CardColor.CLUB), new Card(CardValue.FIVE, CardColor.CLUB), new Card(CardValue.SIX, CardColor.CLUB), new Card(CardValue.ACE, CardColor.CLUB)));
+
+                var party2 = new Party(flushHand, betterFlushHand);
+                assertEquals(betterFlushHand, party2.getWinner());
             }
         }
 
