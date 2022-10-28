e108c6724d1000968bd7e914c47ae17b2e6a888e
loicpalayer <loic.palayer@etu.univ-cotedazur.fr>
2022-10-28 16:10:42 +0200

    feat: allow user input to use icons

--- src/main/java/pokerhand/core/Card.java
+++ src/main/java/pokerhand/core/Card.java
@@ -35,10 +35,10 @@ public record Card(CardValue value, CardColor color) implements Comparable<Card>
 
             CardColor cardColor =
                     switch (color) {
-                        case "tr", "trèfle", "trefle" -> CardColor.CLUB;
-                        case "co", "coeur" -> CardColor.HEART;
-                        case "ca", "carreau" -> CardColor.DIAMOND;
-                        case "pi", "pique" -> CardColor.SPADE;
+                        case "tr", "trèfle", "trefle", "♣" -> CardColor.CLUB;
+                        case "co", "coeur", "♥" -> CardColor.HEART;
+                        case "ca", "carreau", "♦" -> CardColor.DIAMOND;
+                        case "pi", "pique", "♠" -> CardColor.SPADE;
                         default -> throw new IllegalArgumentException(
                                 "CardColor must be between Tr ,Co,Ca,Pi");
                     };
