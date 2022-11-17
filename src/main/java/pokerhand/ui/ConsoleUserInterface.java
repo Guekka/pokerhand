ad4c6d725bbed4b9417df398715b7edf9588d7cb
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2022-11-17 14:46:37 +0100

    feat: add choice selection to ConsoleUserInterface

--- src/main/java/pokerhand/ui/ConsoleUserInterface.java
+++ src/main/java/pokerhand/ui/ConsoleUserInterface.java
@@ -2,6 +2,7 @@ package pokerhand.ui;
 
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.List;
 import java.util.Scanner;
 import pokerhand.core.Hand;
 
@@ -32,6 +33,22 @@ public class ConsoleUserInterface implements UserInterface {
 
     public void displayHand(Hand hand) {
         display(hand.toString());
+    }
+
+    @Override
+    public String getChoice(String message, List<String> choices) {
+        display(message);
+        for (int i = 0; i < choices.size(); i++) {
+            display((i + 1) + " - " + choices.get(i));
+        }
+        display("Enter your choice:");
+
+        String choice = scanner.nextLine();
+        int choiceIndex = Integer.parseInt(choice) - 1;
+        if (choiceIndex < 0 || choiceIndex >= choices.size()) {
+            throw new IllegalArgumentException("Invalid choice");
+        }
+        return choices.get(choiceIndex);
     }
 
     public Hand getHand() {
