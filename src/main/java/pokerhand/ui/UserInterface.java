d5b8601c3e1e7fc7e69cfd722a186c7174d246d1
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2022-11-17 14:47:05 +0100

    docs: add docs to UserInterface

--- src/main/java/pokerhand/ui/UserInterface.java
+++ src/main/java/pokerhand/ui/UserInterface.java
@@ -1,14 +1,35 @@
 package pokerhand.ui;
 
+import java.util.List;
 import pokerhand.core.Hand;
 
 public interface UserInterface {
+    /**
+     * Display a message to the user
+     *
+     * @param message the message to display
+     */
     void display(String message);
 
+    /**
+     * Display an error message to the user
+     *
+     * @param message the error message to display
+     */
     void displayError(String message);
 
+    /**
+     * Reads a hand from the user
+     *
+     * @return the hand entered by the user
+     */
     Hand getHand();
 
+    /**
+     * Display a hand to the user
+     *
+     * @param hand the hand to display
+     */
     void displayHand(Hand hand);
 
     /**
